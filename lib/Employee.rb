1832f8c4e1f8ef8f00b49d23e80514ff700109cf
divbeech <dyadav1006@gmail.com>
2019-09-30 19:08:54 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,27 @@
 class Employee
+    attr_reader :name
+    attr_accessor :salary
 
-end
+    @@all = []
+
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def manager_name
+        Manager.all.select do |manager|
+            manager.employee == self 
+            binding.pry
+        end
+    end
+
+
+
+end # end of class
+
