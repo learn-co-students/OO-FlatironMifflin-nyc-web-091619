b6a0c0cd8783eb26ee66e839f9cdd7d26f075b6a
Danny Lee <dreamer@panix.com>
2019-10-06 22:24:00 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,36 @@
 class Employee
+    attr_reader :name, :salary, :manager_name
+    
+    def initialize
+        @name = name
+        @salary = salary
+        @manager_name = manager_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(amount)
+        self.all.select do |employee|
+            employee.salary > amount
+        end
+    end
+
+    def self.find_by_department(string)
+        # get Manager.all_departments
+        # iterate through and get all managers whose department matches string
+        # get employees of those manager
+        # return first
+    end
+
+    def tax_bracket
+        # call self.paid_over using self.salary as the amount argument
+    end
+    
+            
+
+
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,37 @@
 class Manager
+    attr_reader :name, :department, :age
+    
+    def initialize
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.all.select do |employee|
+            employee.manager_name == self.name
+        end
+    end
+
+    def hire_employee(employee_name, salary)
+        Employee.new(employee_name, salary, self.name)
+    end
+
+    def self.all_departments
+        array = []
+        # get all managers
+        # iterate and add their name/department to array
+        # return array
+    end
+
+    def self.average_age
+        # get all manager objects
+        # iterate through and create array of ages
+        # arr.inject { |sum, el| sum + el }.to_f / arr.size
+    end
 end
